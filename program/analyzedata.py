9231ffcb29d800972192dcdd931adb59dc1f6eaa
Cas <51390039+CasColucci@users.noreply.github.com>
2021-09-15 14:11:20 -0400

    Program was outdated, updated now

--- program/analyzedata.py
+++ program/analyzedata.py
@@ -1,6 +1,77 @@
 import pandas as pd
 import json
 import os
+import math
+import matplotlib.pyplot as plt
+
+
+def mean(arg):
+    val = sum(arg)
+    return val / len(arg)
+
+
+def meanlist(arg):
+    count = 0
+    overnum = 0
+    astronum = 0
+    fakenum = 0
+    finnum = 0
+    othnum = 0
+    selfnum = 0
+    spamnum = 0
+    data = []
+
+    for item in arg:
+        # generate the average score for each bot score
+        count = count + 1
+        overnum = overnum + float(item[0])
+        astronum = astronum + float(item[1])
+        fakenum = fakenum + float(item[2])
+        finnum = finnum + float(item[3])
+        othnum = othnum + float(item[4])
+        selfnum = selfnum + float(item[5])
+        spamnum = spamnum + float(item[6])
+    # calculate the average
+    data.append(overnum / count)
+    data.append(astronum / count)
+    data.append(fakenum / count)
+    data.append(finnum / count)
+    data.append(othnum / count)
+    data.append(selfnum / count)
+    data.append(spamnum / count)
+
+    return data
+
+
+def variance(arg):
+    meanvar = mean(arg)
+    num = 0
+    for item in arg:
+        num += (item - meanvar) ** 2
+    den = len(arg) - 1
+    return num / den
+
+
+def variancelist(arg):
+    means = []
+    data = []
+    for item in arg:
+        meanvar = mean(item)
+        means.append(meanvar)
+        num = 0
+        for i in item:
+            num += (i - meanvar) ** 2
+        den = len(item) - 1
+        data.append(num / den)
+    fulldict = {means[i]: data[i] for i in range(len(means))}
+    return data
+
+
+def standard_deviation(arg):
+    data = []
+    for item in arg:
+        data.append(math.sqrt(variance(item)))
+    return data
 
 
 def analyzedata(query):
@@ -14,63 +85,49 @@ def analyzedata(query):
     self_declared = []
     spammer = []
 
-    # to generate the average score
-    count = 0
-    overnum = 0
-    astronum = 0
-    fakenum = 0
-    finnum = 0
-    othnum = 0
-    selfnum = 0
-    spamnum = 0
-
     filenames = os.listdir("data/" + query)
 
-    for filename in filenames: 
+    for filename in filenames:
         with open("data/{}/{}".format(query, filename)) as infile:
             data = json.load(infile)
 
             for item in data:
                 # append each score
-                overall.append(item['overall'])
-                astroturf.append(item['astroturf'])
-                fake_follower.append(item['fake_follower'])
-                financial.append(item['financial'])
-                other.append(item['other'])
-                self_declared.append(item['self_declared'])
-                spammer.append(item['spammer'])
-
-                # generate the average score for each bot score
-                count = count + 1
-                overnum = overnum + float(item['overall'])
-                astronum = astronum + float(item['astroturf'])
-                fakenum = fakenum + float(item['fake_follower'])
-                finnum = finnum + float(item['financial'])
-                othnum = othnum + float(item['other'])
-                selfnum = selfnum + float(item['self_declared'])
-                spamnum = spamnum + float(item['spammer'])
-
-    # calculate the average
-    aveover = overnum / count
-    aveastro = astronum / count
-    avefake = fakenum / count
-    avefin = finnum / count
-    aveoth = othnum / count
-    aveself = selfnum / count
-    avespam = spamnum / count
+                overall.append(item["overall"])
+                astroturf.append(item["astroturf"])
+                fake_follower.append(item["fake_follower"])
+                financial.append(item["financial"])
+                other.append(item["other"])
+                self_declared.append(item["self_declared"])
+                spammer.append(item["spammer"])
 
     info = [overall, astroturf, fake_follower, financial, other, self_declared, spammer]
-    print(info)
-    df = pd.DataFrame(info)
+    stddev = standard_deviation(info)
+    var = variancelist(info)
+    ave = meanlist(info)
+
+    names = [
+        "Overall",
+        "Astroturf",
+        "Fake Follower",
+        "Financial",
+        "Other",
+        "Self Declared",
+        "Spammer",
+    ]
+
+    df = pd.DataFrame(
+        {
+            "Mean": ave,
+            "Variance": var,
+            "Standard Deviation": stddev,
+        },
+        index=names,
+    )
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(query)
+    ax1.boxplot(info, showfliers=False, labels=names)
 
     print(df)
-
-
-
-    print("\nThe average overall bot score is: " + str(round(aveover, 3)))
-    print("\nThe average astroturf bot score is: " + str(round(aveastro, 3)))
-    print("\nThe average fake follower bot score is: " + str(round(avefake, 3)))
-    print("\nThe average financial bot score is: " + str(round(avefin, 3)))
-    print("\nThe average other bot score is: " + str(round(aveoth, 3)))
-    print("\nThe average self delcared bot score is: " + str(round(aveself, 3)))
-    print("\nThe average spammer bot score is: " + str(round(avespam, 3)))  
+    plt.show()
--- program/main.py
+++ program/main.py
@@ -3,24 +3,30 @@ import analyzedata
 import botometer
 import tweepy
 
-full = True 
-while(full): 
-    print("\nWould you like to...\n A.) Gather more data\n B.) Analyze a topic\n C.) exit")
+full = True
+while full:
+    print(
+        "\nWould you like to...\n A.) Gather more data\n B.) Analyze a topic\n C.) exit"
+    )
     answer = input()
-    if(answer == "A" or answer == "a"): 
+    if answer == "A" or answer == "a":
         # take an input of the desired search
-        print('Enter requested topic search: ')
+        print("Enter requested topic search: ")
         query = input()
-        print('Enter requested sample size: ')
+        print("Enter requested sample size: ")
         size = int(input())
-        print('Enter requested subheader for data (press enter if no subheader desired): ')
+        print(
+            "Enter requested subheader for data (press enter if no subheader desired): "
+        )
         sub = input()
         getdata.getdata(query, size, sub)
-    elif(answer == "B" or answer == "b"):
-        print('Enter data folder name: ')
+        full = False
+    elif answer == "B" or answer == "b":
+        print("Enter data folder name: ")
         query = input()
         analyzedata.analyzedata(query)
-    elif(answer == "C" or answer == "c"):
+        full = False
+    elif answer == "C" or answer == "c":
         full = False
     else:
         print("\nInvalid response!")
